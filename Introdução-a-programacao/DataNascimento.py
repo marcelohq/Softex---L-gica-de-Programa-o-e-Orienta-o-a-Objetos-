20768dfb76dc6c9fd005cd4db27525b36fcd7392
Marcelo <si.marcelosantos@gmail.com>
2022-07-25 02:08:44 -0300

    Adicionando mudanças

--- Introdução-a-programacao/DataNascimento.py
+++ Introdução-a-programacao/DataNascimento.py
@@ -1,4 +1,11 @@
 from logging import exception
+# Desenvolva um programa que recebe do usuário nome completo e ano de nascimento que seja entre 1922 e 2021. 
+# A partir dessas informações, o sistema mostrará o nome do usuário e a idade que completou, ou completará, 
+# no ano atual (2022).
+
+# Caso o usuário não digite um número ou apareça um inválido no campo do ano, o sistema informará 
+# o erro e continuará perguntando até que um valor correto seja preenchido.
+
 
 #Função que recebe a data e obriga o usuário a digitar o tipo de valor correto.
 def dataCorreta(mensagem):
