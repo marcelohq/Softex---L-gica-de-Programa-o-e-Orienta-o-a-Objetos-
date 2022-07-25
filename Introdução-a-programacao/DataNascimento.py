5251576a48205bfaf649c9a3968a000232625a5c
Marcelo <si.marcelosantos@gmail.com>
2022-07-25 02:04:11 -0300

    Adicionando desafio 13: Tratamentos e exceções

---
+++ Introdução-a-programacao/DataNascimento.py
@@ -0,0 +1,29 @@
+from logging import exception
+
+#Funçção que recebe a data e obriga o usuário a digitar o tipo de valor correto.
+def dataCorreta(mensagem):
+
+    while True:
+        try:
+            data = int(input(mensagem))
+        except(TypeError, ValueError):
+            print("Você precisa digitar um valor inteiro para o seu nascimento!")
+            continue
+        else:
+            return data
+
+parar = False
+
+#Pedindo o nome do usuário 
+nome = input("Informe o seu nome: ")
+#Laço que puxa a função, com a condição de parada o ano informado estar entre 1922 e 2022
+while parar == False:
+    data = dataCorreta("Informe em que ano você nasceu: ")
+    if data <1922 or data >2022:
+        print("Você digitou um ano fora do intervalo de 1922 e 2022")
+    else:
+        parar = True
+
+#Saída dos valores de entrada que foram processados, informando nome e quantos anos você terá em 2022.
+print(f"Seu nome é {nome}")
+print("Sua idade em 2022 será ",2022 - data,"anos")
