da69a6f40353a0230b3f995eb0a16e79230daa12
Marcelo <si.marcelosantos@gmail.com>
2022-08-02 15:03:16 -0300

    Adicionando projeto de serializaçao e desserialização em Java

---
+++ Programacão-Orientada-a-Objeto/Serializacao/src/App.java
@@ -0,0 +1,28 @@
+//Crie um exemplo de como funcionam a serialização e a desserialização de um sistema qualquer. Utilize as classes, os objetos e métodos padrões da Java e insira um endereço físico fictício.
+import java.io.*;
+
+public class App {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+
+        Usuario marcelo = new Usuario("Marcelo", 12);
+        
+        //Criando objeto que receberá o arquivo e o endereço físico  | Criando o endereço físico do arquivo que vai receber o fluxo de bytes
+        ObjectOutputStream saidoObjeto = new ObjectOutputStream(new FileOutputStream("src/usuario.serializable"));
+
+        saidoObjeto.writeObject(marcelo); // Serializando o objeto "marcelo"
+
+        saidoObjeto.close(); //Finalizando o processo
+
+        //Criando objeto que receberá o fluxo de bytes   || Pegando o endereço do fluxo do fluxo de bytes
+        ObjectInputStream entradaObjeto = new ObjectInputStream(new FileInputStream("src/usuario.serializable"));
+        
+        Usuario desserializado = (Usuario) entradaObjeto.readObject();// Lendo o fluxo de bytes e atribuindo as informações a um objeto criado, neste caso é o objeto "desserializado"
+        entradaObjeto.close();//Finalizando o processo
+
+        //Mostando os atributos do objeto que foi desserializado. Deve conter as mesmas informações que foram passadas pelo método construtor. Nome: Marcelo, Id: 12.
+        System.out.println("Nome do objeto desserializado: "+desserializado.getNome());
+        System.out.println("Id do objeto desserializado: "+desserializado.getId());
+        
+
+    }
+}
---
+++ Programacão-Orientada-a-Objeto/Serializacao/src/Usuario.java
@@ -0,0 +1,31 @@
+import java.io.Serializable;
+
+//Para fazer a serialização é necessário implementar a interface Serializable
+public class Usuario implements Serializable {
+
+    //Atributos da classe
+    private String nome;
+    private int id; 
+
+    //Método construtor
+    public Usuario(String nome, int id) {
+        this.nome = nome;
+        this.id = id;
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public int getId(){
+        return this.id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+}
